9291640366b749d22c80ecd63eab593b268c7688
ilya <ilya.shuvatov1@gmail.com>
2024-05-19 21:16:01 +0200

    Fix prices and new services

--- dictionaries/types.tsx
+++ dictionaries/types.tsx
@@ -1,118 +1,121 @@
 export interface Root {
-  page: Page
+  page: Page;
 }
 
 export interface Page {
-  home: Home
-  about: About
-  policy: Policy
+  home: Home;
+  about: About;
+  policy: Policy;
 }
 
 export interface Home {
-  navigation: Navigation
-  hero: Hero
-  skills: Skills2
-  projects: Projects2
-  testimonials: Testimonials2
-  contacts: Contacts
-  title: string
-  description: string
+  navigation: Navigation;
+  hero: Hero;
+  skills: Skills2;
+  projects: Projects2;
+  testimonials: Testimonials2;
+  contacts: Contacts;
+  title: string;
+  description: string;
 }
 
 export interface Navigation {
-  home: Home2
-  skills: Skills
-  projects: Projects
-  testimonials: Testimonials
-  "contact me": ContactMe
+  home: Home2;
+  skills: Skills;
+  projects: Projects;
+  testimonials: Testimonials;
+  "contact me": ContactMe;
 }
 
 export interface Home2 {
-  title: string
-  href: string
+  title: string;
+  href: string;
 }
 
 export interface Skills {
-  title: string
-  href: string
+  title: string;
+  href: string;
 }
 
 export interface Projects {
-  title: string
-  href: string
+  title: string;
+  href: string;
 }
 
 export interface Testimonials {
-  title: string
-  href: string
+  title: string;
+  href: string;
 }
 
 export interface ContactMe {
-  title: string
-  href: string
+  title: string;
+  href: string;
 }
 
 export interface Hero {
-  title: string
-  description: string
-  button: string
+  title: string;
+  description: string;
+  button: string;
 }
 
 export interface Skills2 {
-  service: string
-  costs: string
-  seo: string
-  "content marketing": string
-  "social media marketing": string
-  "pay-per-click": string
-  "influencer marketing": string
-  "online advertising": string
-  "reputation management": string
-  "email marketing": string
-  consulting: string
-  month: string
-  hour: string
-  "experience-title": string
-  "experience-description": string
-  "innovative-title": string
-  "innovative-description": string
-  "imaginative-title": string
-  "imaginative-description": string
+  service: string;
+  costs: string;
+  general_marketing: string;
+  marketing_department: string;
+  content_creation: string;
+  content_editing: string;
+  social_media_marketing: string;
+  influencer_marketing: string;
+  pay_per_click: string;
+  seo: string;
+  marketing_strategy: string;
+  online_advertising: string;
+  reputation_management: string;
+  email_marketing: string;
+  month: string;
+  hour: string;
+  "experience-title": string;
+  "experience-description": string;
+  "innovative-title": string;
+  "innovative-description": string;
+  "imaginative-title": string;
+  "imaginative-description": string;
 }
 
 export interface Projects2 {
-  our: string
-  pro: string
-  jects: string
-  description: string
-  "loft-oblaka": string
-  "reelso-mania": string
-  "life-pay": string
-  "holy-posh": string
+  our: string;
+  pro: string;
+  jects: string;
+  description: string;
+  "loft-oblaka": string;
+  "reelso-mania": string;
+  "life-pay": string;
+  "holy-posh": string;
 }
 
 export interface Testimonials2 {
-  title: string
-  "01": string
-  "02": string
-  "03": string
+  title: string;
+  "01": string;
+  "02": string;
+  "03": string;
 }
 
 export interface Contacts {
-  title: string
-  name: string
-  email: string
-  message: string
-  "lets talk": string
+  title: string;
+  name: string;
+  email: string;
+  message: string;
+  "lets talk": string;
 }
 
 export interface About {
-  title: string
-  description: string
+  title: string;
+  description: string;
 }
 
 export interface Policy {
-  privacy: string,
-  conditions: string,
-  cookies: string,
+  privacy: string;
+  conditions: string;
+  cookies: string;
 }
--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,166 +1,227 @@
-import {useMediaQuery} from "@/hooks/useMediaQuery";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import Image from "next/image";
-import {motion} from 'framer-motion';
-import {LineGradient} from "@/app/components/LineGradient";
-import {Root} from "../../../dictionaries/types";
+import { motion } from "framer-motion";
+import { LineGradient } from "@/app/components/LineGradient";
+import { Root } from "../../../dictionaries/types";
 
 export function Skills({ page }: Root) {
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
-  let section = page.home.skills
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  let section = page.home.skills;
   return (
-    <section id='skills' className='pb-24 max-md:pb-5'>
-      <div className='md:flex md:justify-between md:gap-16 mt-32 max-md:mt-10'>
-        <motion.div initial='hidden'
-                    whileInView='visible'
-                    viewport={{once: true, amount: 0.5}}
-                    transition={{duration: 0.5}}
-                    variants={{
-                      hidden: {opacity: 0, x: -50},
-                      visible: {opacity: 1, x: 0}
-                    }}
-                    className='md:w-1/3 relative z-30'>
-            <p className='font-playfair font-semibold text-4xl mb-3'>
-              <span className='text-white'>{section.service} <span className='text-fuchsia-500'>&</span> {section.costs}</span>
-            </p>
-            <LineGradient width='w-1/2'/>
-          <div className='mt-10 mb-7'>
+    <section id="skills" className="pb-24 max-md:pb-5">
+      <div className="md:flex md:justify-between md:gap-16 mt-32 max-md:mt-10">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="md:w-1/3 relative z-30"
+        >
+          <p className="font-playfair font-semibold text-4xl mb-3">
+            <span className="text-white">
+              {section.service} <span className="text-fuchsia-500">&</span>{" "}
+              {section.costs}
+            </span>
+          </p>
+          <LineGradient width="w-1/2" />
+          <div className="mt-10 mb-7">
             <ul>
-              <li className='pb-5'>
+              <li className="pb-5">
+                💡 {section.general_marketing}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €100 / {section.hour}
+                </p>
+              </li>
+              <li className="pb-2">
+                💡 {section.marketing_department}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €6 / {section.hour} (minimum 100 hours/month)
+                </p>
+              </li>
+              <li className="pb-2">
+                💡 {section.content_creation}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €100 / {section.hour}
+                </p>
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €1,500 - €10,000 / {section.month}
+                </p>
+              </li>
+              <li className="pb-2">
+                💡 {section.content_editing}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €100 / {section.hour}
+                </p>
+              </li>
+              <li className="pb-2">
+                💡 {section.social_media_marketing}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €100 / {section.hour}
+                </p>
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €900 - €2,000 / {section.month}
+                </p>
+              </li>
+              <li className="pb-2">
+                💡 {section.influencer_marketing}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €100 / {section.hour}
+                </p>
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €1,000 - €3,000 / {section.month}
+                </p>
+              </li>
+              <li className="pb-2">
+                💡 {section.pay_per_click}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €4,100 - €7,000 / {section.month}
+                </p>
+              </li>
+              <li className="pb-2">
                 💡 {section.seo}
-                <p className='pl-6 text-zinc-300 text-xs'>€1.100 – €4.000 / {section.month}</p>
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €1,100 - €4,000 / {section.month}
+                </p>
               </li>
-              <li className='pb-2'>
-                💡 {section["content marketing"]}
-                <p className='pl-6 text-zinc-300 text-xs'>€1.500 – €10.000 / {section.month}</p>
+              <li className="pb-2">
+                💡 {section.marketing_strategy}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  project + roadmap: €250
+                </p>
               </li>
-              <li className='pb-2'>
-                💡 {section[ "social media marketing"]}
-                <p className='pl-6 text-zinc-300 text-xs'>€900 – €20.000 / {section.month}</p>
+              <li className="pb-2">
+                💡 {section.online_advertising}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €250 / {section.month}
+                </p>
               </li>
-              <li className='pb-2'>
-                💡 {section["pay-per-click"]}
-                <p className='pl-6 text-zinc-300 text-xs'>€4.100 – €7.000 / {section.month}</p>
+              <li className="pb-2">
+                💡 {section.reputation_management}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €250 / {section.month}
+                </p>
               </li>
-              <li className='pb-2'>
-                💡 {section["influencer marketing"]}
-                <p className='pl-6 text-zinc-300 text-xs'>€1.000 – €3.000 / {section.month}</p>
-              </li>
-              <li className='pb-2'>
-                💡 {section["online advertising"]}
-                <p className='pl-6 text-zinc-300 text-xs'>€2.100 – €4.500 / {section.month}</p>
-              </li>
-              <li className='pb-2'>
-                💡 {section["reputation management"]}
-                <p className='pl-6 text-zinc-300 text-xs'>€500 – €4.000 / {section.month}</p>
-              </li>
-              <li className='pb-2'>
-                💡 {section["email marketing"]}
-                <p className='pl-6 text-zinc-300 text-xs'>€300 – €5.000 / {section.month}</p>
-              </li>
-              <li className='pb-2'>
-                💡 {section.consulting}
-                <p className='pl-6 text-zinc-300 text-xs'>€100 / {section.hour}</p>
+              <li className="pb-2">
+                💡 {section.email_marketing}
+                <p className="pl-6 text-zinc-300 text-xs">
+                  €250 / {section.month}
+                </p>
               </li>
             </ul>
           </div>
         </motion.div>
 
-        <div className='mt-16 md:mt-0'>
+        <div className="mt-16 md:mt-0">
           {isAboveMediumScreens ? (
-            <div
-              className='relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 before:w-full before:h-full before:border-2 before:border-fuchsia-500 before:z-[-1]'>
-              <div className='md:order-2 flex justify-center basis-3/5 w-full h-full relative'>
-                <Image src='/skills.jpg' alt='skills image' width={700} height={200}
-                       className='object-cover object-top z-10 w-full relative'/>
-                <div className='absolute z-10 w-full h-full'>
-                  <div
-                    className='absolute bottom-0 left-0 right-0 top-1/2 bg-gradient-to-t from-zinc-900 to-transparent opacity-75 hover:opacity-0 transition-opacity duration-500'>
-                  </div>
+            <div className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 before:w-full before:h-full before:border-2 before:border-fuchsia-500 before:z-[-1]">
+              <div className="md:order-2 flex justify-center basis-3/5 w-full h-full relative">
+                <Image
+                  src="/skills.jpg"
+                  alt="skills image"
+                  width={700}
+                  height={200}
+                  className="object-cover object-top z-10 w-full relative"
+                />
+                <div className="absolute z-10 w-full h-full">
+                  <div className="absolute bottom-0 left-0 right-0 top-1/2 bg-gradient-to-t from-zinc-900 to-transparent opacity-75 hover:opacity-0 transition-opacity duration-500"></div>
                 </div>
               </div>
             </div>
           ) : (
-            <div className='md:order-2 flex justify-center basis-3/5 w-full h-full relative'>
-              <Image src='/skills.jpg' alt='skills image' width={700} height={200}
-                     className='object-cover object-top z-10 w-full relative'/>
-              <div className='absolute z-10 w-full h-full'>
-                <div
-                  className='absolute bottom-0 left-0 right-0 top-1/2 bg-gradient-to-t from-zinc-900 to-transparent opacity-75 hover:opacity-0 transition-opacity duration-500'>
-                </div>
+            <div className="md:order-2 flex justify-center basis-3/5 w-full h-full relative">
+              <Image
+                src="/skills.jpg"
+                alt="skills image"
+                width={700}
+                height={200}
+                className="object-cover object-top z-10 w-full relative"
+              />
+              <div className="absolute z-10 w-full h-full">
+                <div className="absolute bottom-0 left-0 right-0 top-1/2 bg-gradient-to-t from-zinc-900 to-transparent opacity-75 hover:opacity-0 transition-opacity duration-500"></div>
               </div>
             </div>
           )}
         </div>
       </div>
 
-        {/*SKILLS*/}
-        <div className="md:flex md:justify-between mt-16 gap-32">
-          {/*EXPERIENCE*/}
-          <motion.div initial='hidden'
-                      whileInView='visible'
-                      viewport={{once: true, amount: 0.5}}
-                      transition={{duration: 0.5}}
-                      variants={{
-                        hidden: {opacity: 0, y: 50},
-                        visible: {opacity: 1, y: 0}
-                      }}
-                      className='md:w-1/3 mt-10'>
-            <div className='relative h-32'>
-              <div className="z-10">
-                <p className='font-playfair font-semibold text-5xl'>01</p>
-                <p className='font-playfair font-semibold text-3xl mt-3'>{section["experience-title"]}</p>
-              </div>
-              <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]"/>
+      {/*SKILLS*/}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/*EXPERIENCE*/}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          className="md:w-1/3 mt-10"
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">01</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                {section["experience-title"]}
+              </p>
             </div>
-            <p className="mt-5">
-              {section["experience-description"]}
-            </p>
-          </motion.div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">{section["experience-description"]}</p>
+        </motion.div>
 
-          {/*INOVATIVE*/}
-          <motion.div initial='hidden'
-                      whileInView='visible'
-                      viewport={{once: true, amount: 0.5}}
-                      transition={{delay: 0.2, duration: 0.5}}
-                      variants={{
-                        hidden: {opacity: 0, y: 50},
-                        visible: {opacity: 1, y: 0}
-                      }}
-                      className='md:w-1/3 mt-10'>
-            <div className='relative h-32'>
-              <div className="z-10">
-                <p className='font-playfair font-semibold text-5xl'>02</p>
-                <p className='font-playfair font-semibold text-3xl mt-3'>{section["innovative-title"]}</p>
-              </div>
-              <div className="w-1/2 md:w-3/4 h-32 bg-fuchsia-500 absolute right-0 top-0 z-[-1]"/>
+        {/*INOVATIVE*/}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          className="md:w-1/3 mt-10"
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">02</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                {section["innovative-title"]}
+              </p>
             </div>
-            <p className="mt-5">
-              {section["innovative-description"]}
-            </p>
-          </motion.div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-fuchsia-500 absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">{section["innovative-description"]}</p>
+        </motion.div>
 
-          {/*IMAGINATIVE*/}
-          <motion.div initial='hidden'
-                      whileInView='visible'
-                      viewport={{once: true, amount: 0.5}}
-                      transition={{delay: 0.4, duration: 0.5}}
-                      variants={{
-                        hidden: {opacity: 0, y: 50},
-                        visible: {opacity: 1, y: 0}
-                      }}
-                      className='md:w-1/3 mt-10'>
-            <div className='relative h-32'>
-              <div className="z-10">
-                <p className='font-playfair font-semibold text-5xl'>03</p>
-                <p className='font-playfair font-semibold text-3xl mt-3'>{section["imaginative-title"]}</p>
-              </div>
-              <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]"/>
+        {/*IMAGINATIVE*/}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          className="md:w-1/3 mt-10"
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">03</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                {section["imaginative-title"]}
+              </p>
             </div>
-            <p className="mt-5">
-              {section["imaginative-description"]}
-            </p>
-          </motion.div>
-        </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">{section["imaginative-description"]}</p>
+        </motion.div>
+      </div>
     </section>
-  )
+  );
 }
